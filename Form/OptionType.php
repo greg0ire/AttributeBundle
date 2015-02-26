e44474bb4f0bb0e0efabf99360bb53bcd855b28a
Grégoire Paris <gparis@universcine.com>
2015-02-26 12:33:29 +0100

    resurrect option type

---
+++ Form/OptionType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Padam87\AttributeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class OptionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', 'text', array());
+    }
+
+    public function getName()
+    {
+        return 'attribute_option';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Padam87\AttributeBundle\Entity\Option',
+        ));
+    }
+}
